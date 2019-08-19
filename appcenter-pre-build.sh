d2528cb208c534eb70972ead1203bfdb65333588
Ivan Nosar <v-ivnosa@microsoft.com>
2019-08-19 12:03:35 +0300

    Add pre-build

---
+++ appcenter-pre-build.sh
@@ -0,0 +1,6 @@
+echo "Mono version:"
+mono --version
+
+echo ".NET Core version: $(dotnet --version)"
+
+echo "\nSDK version: $(cat /Library/Frameworks/Xamarin.iOS.framework/Versions/Current/Version)"
