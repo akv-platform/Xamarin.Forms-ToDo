4cec6a27e4d924354916564ff07b2edc7d15379c
Ivan Nosar <v-ivnosa@microsoft.com>
2019-08-19 17:44:56 +0300

    final version

--- Todo.Android/appcenter-post-clone.sh
+++ Todo.Android/appcenter-post-clone.sh
@@ -1,22 +1,12 @@
-echo "Mono version:"
-mono --version
-
-echo ".NET Core version: $(dotnet --version)"
-
-echo "SDK version: $(cat /Library/Frameworks/Xamarin.iOS.framework/Versions/Current/Version)"
-
-echo "Available .NET Core runtimes:"
-dotnet --list-runtimes
-
 DOTNET_VERSION="2.2.105"
 echo "======== Switch .Net Core version to ${DOTNET_VERSION} ========"
 
-sudo rm -rf "/Users/vsts/.dotnet"
+# sudo rm -rf "/Users/vsts/.dotnet"
 
 wget https://dot.net/v1/dotnet-install.sh
 chmod +x dotnet-install.sh
-./dotnet-install.sh --version ${DOTNET_VERSION} #--install-dir "/Users/Shared/.dotnet"
+./dotnet-install.sh --version ${DOTNET_VERSION} --install-dir "/Users/Shared/.dotnet"
 
-export PATH="/Users/vsts/.dotnet:$PATH" 
-# Throw `PATH` to the next step (AzDO specific)
+export PATH="/Users/Shared/.dotnet:$PATH" 
+# Throw `PATH` to the next steps (AzDO specific)
 echo '##vso[task.prependpath]'$PATH
