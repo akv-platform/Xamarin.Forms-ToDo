88cfaaf405dfbbcc858581863e5839d2f33455b5
Ivan Nosar <v-ivnosa@microsoft.com>
2019-08-19 16:55:05 +0300

    try

--- Todo.Android/appcenter-pre-build.sh
+++ Todo.Android/appcenter-pre-build.sh
@@ -8,19 +8,5 @@ echo "SDK version: $(cat /Library/Frameworks/Xamarin.iOS.framework/Versions/Curr
 echo "Available .NET Core runtimes:"
 dotnet --list-runtimes
 
-DOTNET_VERSION="2.2.105"
-echo "======== Switch .Net Core version to ${DOTNET_VERSION} ========"
-
-wget https://dot.net/v1/dotnet-install.sh
-chmod +x dotnet-install.sh
-./dotnet-install.sh --version ${DOTNET_VERSION} --install-dir "/Users/Shared/.dotnet"
-
-PATH="/Users/Shared/.dotnet:${PATH}"
-# Throw `PATH` to the next step (AzDO specific)
-echo '##vso[task.setvariable variable=PATH;]'$PATH
-echo "PATH:"
+echo "Path:"
 echo $PATH
-
-echo "====================================================="
-
-echo ".NET Core version: $(dotnet --version)"
