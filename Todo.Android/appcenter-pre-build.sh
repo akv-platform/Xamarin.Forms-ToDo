cca9ed523cac0105ec430ccb9d37c1cc09310fe5
Ivan Nosar <v-ivnosa@microsoft.com>
2019-08-19 12:22:09 +0300

    Restored pre-build

---
+++ Todo.Android/appcenter-pre-build.sh
@@ -0,0 +1,9 @@
+echo "Mono version:"
+mono --version
+
+echo ".NET Core version: $(dotnet --version)"
+
+echo "SDK version: $(cat /Library/Frameworks/Xamarin.iOS.framework/Versions/Current/Version)"
+
+echo "Available .NET Core runtimes:"
+dotnet --list-runtimes
