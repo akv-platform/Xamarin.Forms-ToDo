ef82b148d16416f423e8fa1d23a666c563319dfa
Ivan Nosar <v-ivnosa@microsoft.com>
2019-08-19 15:45:24 +0300

    install dotnet

--- Todo.Android/appcenter-post-clone.sh
+++ Todo.Android/appcenter-post-clone.sh
@@ -11,13 +11,12 @@ dotnet --list-runtimes
 DOTNET_VERSION="2.2.105"
 echo "======== Switch .Net Core version to ${DOTNET_VERSION} ========"
 
-echo '{"sdk":{"version":"'"${DOTNET_VERSION}"'"}}' > global.json
+wget https://dot.net/v1/dotnet-install.sh
+chmod +x dotnet-install.sh
+./dotnet-install.sh --version ${DOTNET_VERSION}
+
+export PATH="${~}/.dotnet:${PATH}"
 
 echo "====================================================="
 
-echo "Mono version:"
-mono --version
-
 echo ".NET Core version: $(dotnet --version)"
-
-echo "SDK version: $(cat /Library/Frameworks/Xamarin.iOS.framework/Versions/Current/Version)"
