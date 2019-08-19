327ebc3f4c9bde930271187d7f6d3cd052fbd4eb
Ivan Nosar <v-ivnosa@microsoft.com>
2019-08-19 16:24:24 +0300

    PATH

--- Todo.Android/appcenter-post-clone.sh
+++ Todo.Android/appcenter-post-clone.sh
@@ -24,3 +24,4 @@ echo $PATH
 echo "====================================================="
 
 echo ".NET Core version: $(dotnet --version)"
+# /Users/vsts/.dotnet/tools:/Users/vsts/hostedtoolcache/dotnet:/Users/vsts/Library/Android/sdk/android-ndk-r15c:/Library/Frameworks/Mono.framework/Versions/6_0_0/bin:/Users/vsts/.dotnet/tools:/Users/vsts/hostedtoolcache/dotnet:/Users/Shared/.dotnet:/Users/vsts/.dotnet/tools:/Users/vsts/hostedtoolcache/dotnet:/Library/Frameworks/Mono.framework/Versions/4/bin:/usr/local/lib/ruby/gems/2.6.0/bin:/usr/local/opt/ruby/bin:/usr/local/opt/curl/bin:/usr/local/bin:/usr/local/sbin:/Users/vsts/bin:/Users/vsts/.yarn/bin:/usr/local/go/bin:/Users/vsts/Library/Android/sdk/tools:/Users/vsts/Library/Android/sdk/platform-tools:/Users/vsts/Library/Android/sdk/ndk-bundle:/Library/Frameworks/Mono.framework/Versions/Current/Commands:/usr/bin:/bin:/usr/sbin:/sbin:/Users/vsts/.azcopy
