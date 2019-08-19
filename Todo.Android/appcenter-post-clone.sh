3a8b09e19ce857926e23bc094956aeeeba78a94d
Ivan Nosar <v-ivnosa@microsoft.com>
2019-08-19 17:25:45 +0300

    try

--- Todo.Android/appcenter-post-clone.sh
+++ Todo.Android/appcenter-post-clone.sh
@@ -17,9 +17,9 @@ wget https://dot.net/v1/dotnet-install.sh
 chmod +x dotnet-install.sh
 ./dotnet-install.sh --version ${DOTNET_VERSION} #--install-dir "/Users/Shared/.dotnet"
 
-# PATH="/Users/Shared/.dotnet:${PATH}"
+PATH="/Users/vsts/.dotnet:${PATH}"
 # Throw `PATH` to the next step (AzDO specific)
-# echo '##vso[task.setvariable variable=PATH;]'$PATH
+echo '##vso[task.setvariable variable=PATH;]'$PATH
 echo "PATH:"
 echo $PATH
 
