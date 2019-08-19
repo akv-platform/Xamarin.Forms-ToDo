434a0c76d10fc19e3aa0b849363a89cce3a636ec
Ivan Nosar <v-ivnosa@microsoft.com>
2019-08-19 17:23:29 +0300

    try

--- Todo.Android/appcenter-post-clone.sh
+++ Todo.Android/appcenter-post-clone.sh
@@ -11,15 +11,15 @@ dotnet --list-runtimes
 DOTNET_VERSION="2.2.105"
 echo "======== Switch .Net Core version to ${DOTNET_VERSION} ========"
 
-sudo rm -rf ~/.dotnet/sdk
+sudo rm -rf "/Users/vsts/.dotnet"
 
 wget https://dot.net/v1/dotnet-install.sh
 chmod +x dotnet-install.sh
 ./dotnet-install.sh --version ${DOTNET_VERSION} #--install-dir "/Users/Shared/.dotnet"
 
-PATH="/Users/Shared/.dotnet:${PATH}"
+# PATH="/Users/Shared/.dotnet:${PATH}"
 # Throw `PATH` to the next step (AzDO specific)
-echo '##vso[task.setvariable variable=PATH;]'$PATH
+# echo '##vso[task.setvariable variable=PATH;]'$PATH
 echo "PATH:"
 echo $PATH
 
