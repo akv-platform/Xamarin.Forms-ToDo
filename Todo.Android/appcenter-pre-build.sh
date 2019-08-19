f5bdde631e600dbdef477d037538e92b94976b74
Ivan Nosar <v-ivnosa@microsoft.com>
2019-08-19 12:16:03 +0300

    Updated pre-build

--- Todo.Android/appcenter-pre-build.sh
+++ Todo.Android/appcenter-pre-build.sh
@@ -3,4 +3,21 @@ mono --version
 
 echo ".NET Core version: $(dotnet --version)"
 
-echo "\nSDK version: $(cat /Library/Frameworks/Xamarin.iOS.framework/Versions/Current/Version)"
+echo "SDK version: $(cat /Library/Frameworks/Xamarin.iOS.framework/Versions/Current/Version)"
+
+echo "Available .NET Core runtimes:"
+dotnet --list-runtimes
+
+DOTNET_VERSION="2.2.105"
+echo "======== Switch .Net Core version to ${DOTNET_VERSION} ========"
+
+echo '{"sdk":{"version":"'"${DOTNET_VERSION}"'"}}' > global.json
+
+echo "====================================================="
+
+echo "Mono version:"
+mono --version
+
+echo ".NET Core version: $(dotnet --version)"
+
+echo "SDK version: $(cat /Library/Frameworks/Xamarin.iOS.framework/Versions/Current/Version)"
